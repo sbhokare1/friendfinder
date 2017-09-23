bd2b58531f16b60d59c34247888443149d294470
sbhokare1 <32080845+sbhokare1@users.noreply.github.com>
2017-09-23 18:44:26 -0400

    integrated camera

--- app/src/main/java/com/example/shub/friendfinder/MainActivity.java
+++ app/src/main/java/com/example/shub/friendfinder/MainActivity.java
@@ -8,27 +8,57 @@ import android.widget.TextView;
 import android.content.Intent;
 import android.widget.ImageView;
 import android.provider.MediaStore;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 
 public class MainActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
-
+    ImageView image_rec;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button image_cap = (Button) findViewById(R.id.button1);
+        Button image_gal = (Button) findViewById(R.id.button_gallery);
+        image_rec = (ImageView) findViewById(R.id.imageView3);
 
         image_cap.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 dispatchTakePictureIntent();
             }
         });
+        image_gal.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                dispatchGetPictureIntent();
+            }
+        });
+
     }
 
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    private void dispatchGetPictureIntent() {
+        Intent getPictureIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(getPictureIntent, 1);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            image_rec.setImageBitmap(imageBitmap);
+        }
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            image_rec.setImageBitmap(imageBitmap);
         }
     }
 }
