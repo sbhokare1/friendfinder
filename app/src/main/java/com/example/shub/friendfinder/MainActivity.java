d27f2b6bdc2c323079aa960d3d8519196518c9c7
sbhokare1 <32080845+sbhokare1@users.noreply.github.com>
2017-09-23 15:59:14 -0400

    Friend finder app

--- app/src/main/java/com/example/shub/friendfinder/MainActivity.java
+++ app/src/main/java/com/example/shub/friendfinder/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.shub.friendfinder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.content.Intent;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    
 }
+
