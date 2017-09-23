515f0bebac17bf185d7f320af05d634f8640438e
sbhokare1 <32080845+sbhokare1@users.noreply.github.com>
2017-09-23 16:38:19 -0400

    integrated camera

--- app/src/main/java/com/example/shub/friendfinder/MainActivity.java
+++ app/src/main/java/com/example/shub/friendfinder/MainActivity.java
@@ -7,15 +7,32 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.content.Intent;
 import android.widget.ImageView;
+import android.provider.MediaStore;
 
 public class MainActivity extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button image_cap = (Button) findViewById(R.id.button1);
+
+        image_cap.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
     }
 
-    
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
 }
+
+
+
 
